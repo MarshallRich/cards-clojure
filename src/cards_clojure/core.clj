089e7065aca5b719a073a87e6bfe82fcf6a4f079
MarshallRich <rlmarsh@g.clemson.edu>
2016-03-28 14:42:55 -0400

    added additional filter operations

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -5,6 +5,7 @@
 
 (def ranks (range 1 14))
 (def rank-names {1 :ace 11 :jack 12 :queen 13 :king})
+(def rank-num {:ace 1 :jack 11 :queen 12 :king 13})
 
 (defn create-deck []
   (set
@@ -24,11 +25,35 @@
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
 
+(defn straight? [hand]
+  (let [
+        hand-check (sort
+                     (set
+                       (for [card hand]
+                        (get rank-num (:rank card) (:rank card)))))]
+       (and (= 4 (count hand-check))
+            (= 3 (- (last hand-check) (first hand-check))))))
+
+(defn straight-flush? [hand]
+  (and (flush? hand)
+       (straight? hand)))
+  
+
+(defn four-of-kind? [hand]
+  (= 1 (count (set (map :rank hand)))))
+
+(defn three-of-kind? [hand]
+  (some #(= 3 %) (vals (frequencies (map :rank hand)))))
+
+(defn two-pair? [hand]
+  (and (= 2 (count (vals (frequencies (map :rank hand)))))  
+       (= (first (vals (frequencies (map :rank hand)))) (last (vals (frequencies (map :rank hand))))))) 
+
 (defn -main []
   (time
     (let [deck (create-deck)
           hands (create-hands deck)
-          flush-hands (filter flush? hands)]
-      (count flush-hands))))
+          check-hands (filter two-pair? hands)]
+      (count check-hands))))
     
   
